32d31f14902786e57633ccb6809c903e5c4f4c2c
sneha  pasi <82055349+sneha1967@users.noreply.github.com>
2022-11-25 21:14:19 +0530

    Add files via upload

---
+++ 3oct.py
@@ -0,0 +1,45 @@
+print("hello world")
+a=10
+print(id(a))
+b=10
+print(id(b))
+a=20
+b=20
+c="python"
+d="python"
+e=[1,2,3]
+f=[1,2,3]
+print(id(e))
+print(id(f))
+print(type(e))
+print(type(a))
+#operators
+a=20
+b=20
+print(a+b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+print(a==b)
+d=True
+e=False
+print(d or e)
+print(d and e)
+print(d and d or e)
+#membership
+x='hello sneha'
+print('n' in x)
+print('n' not in x)
+#identity
+x=5
+y=5
+x2='sneha'
+y2='sneha'
+print(x is y)
+print(x is not y)
+z={1:'a'}
+print(type(z))
+a=set()
+print(type(a))
